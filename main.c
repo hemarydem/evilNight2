e6a173877613e3805bac7d54660932f15b689055
R H <m.rehamed@gmail.com>
2020-11-27 22:50:49 +0100

    compile

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
-#include<SDL2>
-#include<stdio>
+#include<SDL2/SDL.h>
+#include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<stdint.h>
 
-int main(int argc, char ** argv) {
-    return 0;
+//gcc main.c $(sdl2-config --cflags --libs) 
+
+int main(int argc, char *argv[]) {
+    if(0 != SDL_Init(SDL_INIT_VIDEO)) {
+        fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    SDL_Quit();
+    return EXIT_SUCCESS;
 }
