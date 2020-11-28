1ee2bb4e4d07243d975fb6731635acc7da1ffdda
R H <m.rehamed@gmail.com>
2020-11-28 08:51:22 +0100

    fenètre ok

--- main.c
+++ main.c
@@ -3,12 +3,13 @@
 #include<stdlib.h>
 #include<string.h>
 #include<stdint.h>
+#include<stdbool.h> 
 
 //gcc main.c $(sdl2-config --cflags --libs) 
 
 int main(int argc, char *argv[]) {
     SDL_Window * win = NULL;
-    SDL_Window * win2 = NULL;
+    SDL_Renderer* rendu = NULL;
     if(0 != SDL_Init(SDL_INIT_VIDEO)) {
         fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
         return EXIT_FAILURE;
@@ -19,28 +20,34 @@ int main(int argc, char *argv[]) {
                         800,
                         600,
                         SDL_WINDOW_SHOWN);
-    win2 = SDL_CreateWindow("secondWindow",
-                        SDL_WINDOWPOS_CENTERED,
-                        SDL_WINDOWPOS_CENTERED,
-                        800,
-                        600,
-                        SDL_WINDOW_SHOWN);                  
-                        
+    
     if(NULL == win){
         fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
         SDL_Quit();
         return EXIT_FAILURE;
     }
-    if(NULL == win2){
-        fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
+    rendu = SDL_CreateRenderer(win,-1,SDL_RENDERER_ACCELERATED);
+    if(NULL == rendu) {
+        fprintf(stderr, "Erreur SDL_CreateRenderer : %s", SDL_GetError());
+        SDL_DestroyRenderer(rendu);
         SDL_DestroyWindow(win);
-        SDL_DestroyWindow(win2);
         SDL_Quit();
-        return EXIT_FAILURE;
     }
+   
     /* On agit sur la fenêtre ici */
-    SDL_Delay(3000);
-    SDL_DestroyWindow(win2);
+
+    //SDL_delay(3000);
+    //https://stackoverflow.com/questions/34424816/sdl-window-does-not-show
+    bool isquit = false;
+    SDL_Event event;
+    while (!isquit) {
+        if (SDL_PollEvent( & event)) {
+            if (event.type == SDL_QUIT) {
+                isquit = true;
+            }
+        }
+    }
+
     SDL_DestroyWindow(win);
     SDL_Quit();
     return EXIT_SUCCESS;
