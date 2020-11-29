5511b1475a34b8ec83e86ca9af8820d80dc70d8f
R H <m.rehamed@gmail.com>
2020-11-29 22:36:12 +0100

    sauve avant test texture

--- main.c
+++ main.c
@@ -7,7 +7,8 @@
 //gcc main.c $(sdl2-config --cflags --libs) 
 int main(int argc, char *argv[]) {
     SDL_Window * win = NULL; 
-    SDL_Renderer* rendu = NULL;
+    SDL_Renderer * rendu = NULL;
+    SDL_Texture * text = NULL;
     if(0 != SDL_Init(SDL_INIT_VIDEO)) {
         fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
         return EXIT_FAILURE;
@@ -30,55 +31,38 @@ int main(int argc, char *argv[]) {
         SDL_DestroyWindow(win);
         SDL_Quit();
     }
+    text = SDL_CreateTexture(rendu, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 200, 200);
+    if(NULL == text) {
+        fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
+        SDL_DestroyTexture(text);
+        SDL_DestroyRenderer(rendu);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+    }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
     if(0 != SDL_SetRenderDrawColor(rendu,255, 255, 220, SDL_ALPHA_OPAQUE)) {//preparing the color
         fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
         SDL_DestroyRenderer(rendu);
         SDL_DestroyWindow(win);
         SDL_Quit();
     }
-    
     if(0 != SDL_RenderClear(rendu)){// put the background color on the renderer
         fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
         SDL_DestroyRenderer(rendu);
         SDL_DestroyWindow(win);
         SDL_Quit();
     }
-    //from there we can draw on the renderer
-    https://stackoverflow.com/questions/18914960/how-to-iterate-over-of-an-array-of-structures/18914961
-    SDL_SetRenderDrawColor(rendu, 0, 0, 0, SDL_ALPHA_OPAQUE);//put the color black for next things will be draw
-    SDL_Rect rect[48];// creat 48 rect
-    SDL_Rect *ptr = rect;// we need ptr for the loop
-    int x = 0;//set the position
-    int y = 0; 
-    int decale = 0;//each line need to switch the colore rythme
-    for (size_t i = 0; i < 48; i++) {
-        if(x > 700) {// it's the last rect got to the line under
-            x = 0;
-            y += 100;
-            if (decale == 0){// to stop the alternate the color must same as last of the upper line
-                decale =1;
-            }else{
-                decale = 0;
-            } 
-        }
-        if (decale == 1){
-            ptr[i].x = x;
-            ptr[i].y = y;
-            ptr[i].h = 100;
-            ptr[i].w = 100;
-            SDL_RenderFillRect(rendu, &ptr[i]); 
-            decale = 0;
-        } else {
-            ptr[i].x = x;
-            ptr[i].y = y;
-            ptr[i].h = 100;
-            ptr[i].w = 100;
-            SDL_RenderDrawRect(rendu, &ptr[i]);
-            decale = 1;
-        }
-        x += 100;
-    }
+
+    //from there we can draw 
+    SDL_Rect rect = {50, 50, 100, 100};
+    SDL_SetRenderDrawColor(rendu, 150, 0, 150, 255);
+    SDL_SetRenderTarget(rendu, text);
+    SDL_RenderFillRect(rendu, &rect);
+    SDL_SetRenderTarget(rendu, NULL);
+
     SDL_RenderPresent(rendu);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     bool isquit = false;
     SDL_Event event;
     while (!isquit) {
@@ -88,6 +72,8 @@ int main(int argc, char *argv[]) {
             }
         }
     }
+    SDL_DestroyTexture(text);
+    SDL_DestroyRenderer(rendu);
     SDL_DestroyWindow(win);
     SDL_Quit();
     return EXIT_SUCCESS;
