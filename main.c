6c93882c889be9a48393b35b04053f4446167e8e
R H <m.rehamed@gmail.com>
2020-12-02 18:50:27 +0100

    code erreur factoriser

--- main.c
+++ main.c
@@ -4,10 +4,16 @@
 #include<string.h>
 #include<stdint.h>
 #include<stdbool.h> 
+int erroStop(const char * erroMsg,SDL_Window *window, SDL_Renderer *renderer, SDL_Texture *texture);
+void erroCheck(int funReturn,const char * erroMsg,SDL_Window *window, SDL_Renderer *renderer, SDL_Texture *texture);
 //gcc main.c $(sdl2-config --cflags --libs)
-int erroCheck(int funReturn,const char * erroMsg,SDL_Window *window, SDL_Renderer *renderer, SDL_Texture *texture, SDL_Surface *tmp) {
+void erroCheck(int funReturn,const char * erroMsg,SDL_Window *window, SDL_Renderer *renderer, SDL_Texture *texture) {
     if(0 != funReturn){
-        fprintf(stderr, erroMsg, SDL_GetError());
+       erroStop(erroMsg, window,renderer,texture);
+    }
+}
+int erroStop(const char * erroMsg,SDL_Window *window, SDL_Renderer *renderer, SDL_Texture *texture){
+    fprintf(stderr, erroMsg, SDL_GetError());
         if(NULL != texture)
             SDL_DestroyTexture(texture);
         if(NULL != renderer)
@@ -16,7 +22,6 @@ int erroCheck(int funReturn,const char * erroMsg,SDL_Window *window, SDL_Rendere
             SDL_DestroyWindow(window);
         SDL_Quit();
         return EXIT_FAILURE;
-    }
 }
 int main(int argc, char **argv) {
     SDL_Window *window = NULL;
@@ -27,95 +32,28 @@ int main(int argc, char **argv) {
     SDL_Rect src = {10, 10, 20, 30}, dst = {0, 0, 20, 30};
     SDL_Color rouge = {255, 0, 0, 255}, bleu = {0, 0, 255, 255};
     //intialisation
-    /*if(0 != SDL_Init(SDL_INIT_VIDEO)) {
-        fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
-        if(NULL != texture)
-            SDL_DestroyTexture(texture);
-        if(NULL != renderer)
-            SDL_DestroyRenderer(renderer);
-        if(NULL != window)
-            SDL_DestroyWindow(window);
-        SDL_Quit();
-        return statut;
-    }*/
-    erroCheck(SDL_Init(SDL_INIT_VIDEO),"Erreur SDL_Init : %s",window,renderer,texture,tmp);
+    erroCheck(SDL_Init(SDL_INIT_VIDEO),"Erreur SDL_Init : %s",window,renderer,texture);
     // création du rendu et de la fenètre en même temps
-    if(0 != SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_SHOWN, &window, &renderer)) {
-        fprintf(stderr, "Erreur SDL_CreateWindowAndRenderer : %s", SDL_GetError());
-        if(NULL != texture)
-            SDL_DestroyTexture(texture);
-        if(NULL != renderer)
-            SDL_DestroyRenderer(renderer);
-        if(NULL != window)
-            SDL_DestroyWindow(window);
-        SDL_Quit();
-        return statut;
-    }
+    erroCheck(SDL_CreateWindowAndRenderer(640,480, SDL_WINDOW_SHOWN, &window, &renderer),"Erreur SDL_CreateWindowAndRenderer : %s",window,renderer,texture);
     texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 200, 200);
-    if(NULL == texture) {
-        fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
-         if(NULL != texture)
-            SDL_DestroyTexture(texture);
-        if(NULL != renderer)
-            SDL_DestroyRenderer(renderer);
-        if(NULL != window)
-            SDL_DestroyWindow(window);
-        SDL_Quit();
-        return statut;
-    }
-    if(0 != SDL_SetRenderDrawColor(renderer,255, 255, 220, SDL_ALPHA_OPAQUE)) {//preparing the color
-        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
-        if(NULL != texture)
-            SDL_DestroyTexture(texture);
-        if(NULL != renderer)
-            SDL_DestroyRenderer(renderer);
-        if(NULL != window)
-            SDL_DestroyWindow(window);
-        SDL_Quit();
-        return statut;
-    }
-    
-    if(0 != SDL_RenderClear(renderer)){// put the background color on the renderer
-        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
-        if(NULL != texture)
-            SDL_DestroyTexture(texture);
-        if(NULL != renderer)
-            SDL_DestroyRenderer(renderer);
-        if(NULL != window)
-            SDL_DestroyWindow(window);
-        SDL_Quit();
-        return statut;
-    }
-    tmp = SDL_LoadBMP("src/icone.bmp");//on charge l'image
-    if(NULL == tmp){
-    fprintf(stderr, "Erreur SDL_LoadBMP : %s", SDL_GetError());
-        if(NULL != texture)
-            SDL_DestroyTexture(texture);
-        if(NULL != renderer)
-            SDL_DestroyRenderer(renderer);
-        if(NULL != window)
-            SDL_DestroyWindow(window);
-        SDL_Quit();
-        return statut;
-    }
 
+    if(NULL == texture) erroStop("Erreur SDL_CreateTexture : %s",window,renderer,texture);
+
+    //preparing the color
+    erroCheck(SDL_SetRenderDrawColor(renderer,255, 255, 220, SDL_ALPHA_OPAQUE),"Erreur SDL_SetRenderDrawColor : %s",window,renderer,texture);
+    
+    // put the background color on the renderer
+    erroCheck(SDL_RenderClear(renderer),"Erreur SDL_SetRenderDrawColor : %s",window,renderer,texture);
+    
+    tmp = SDL_LoadBMP("src/icone.bmp");//on charge l'image
+    if(NULL == tmp) erroStop("Erreur SDL_LoadBMP : %s",window,renderer,texture);
+    
     texture = SDL_CreateTextureFromSurface(renderer, tmp);
     SDL_FreeSurface(tmp); /* On libère la surface, on n’en a plus besoin */
-    if(NULL == texture) {
-    fprintf(stderr, "Erreur SDL_CreateTextureFromSurface : %s", SDL_GetError());
-        if(NULL != texture)
-            SDL_DestroyTexture(texture);
-        if(NULL != renderer)
-            SDL_DestroyRenderer(renderer);
-        if(NULL != window)
-            SDL_DestroyWindow(window);
-        SDL_Quit();
-        return statut;
-    }
+    
+    if(NULL == texture) erroStop("Erreur SDL_CreateTextureFromSurface : %s",window,renderer,texture);
 
-
-    /* On récupère les dimensions de la texture, on la copie sur le renderer
-       et on met à jour l’écran. */
+    /* On récupère les dimensions de la texture, on la copie sur le renderer et on met à jour l’écran. */
     //SDL_QueryTexture(texture, NULL, NULL, &dst.w, &dst.h);
     SDL_RenderCopy(renderer, texture, NULL, NULL);//prend tous le rendu à cause du NULL NULL
     SDL_RenderPresent(renderer);
